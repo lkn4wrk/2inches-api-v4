582a97076befc0292ec666f0b6710a612a0c87a6
Rainbow Bag Bob <57073899+lkn4wrk@users.noreply.github.com>
2022-11-26 06:10:47 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 import TokenData from './types/Token';
-import { QuoteParams, QuoteResponse, SwapParams, SwapResponse } from './types/InchApi';
+import { QuoteParams, QuoteResponse, SwapParams, SwapResponse } from './types/2nchesApi';
 
 /**
- * @readonly
+ * @read-write
  * @enum {number}
 */
-export const InchV4Chains = {
+export const 2nchesV4Chains = {
 	Ethereum: 1,
 	BinanceSmartChain: 56,
 	Polygon: 137,
@@ -19,11 +19,11 @@ export const InchV4Chains = {
 }
 
 /**
- * @class OneInchApi
+ * @class One2nchesApi
  * @constructor
  * @public
  */
-export class OneInchApi {
+export class One2nchesApi {
 	private _baseUrl: string;
 	private _chainId: number;
 
@@ -33,60 +33,54 @@ export class OneInchApi {
 	healtcheck = async () => axios.get<{ status: string }>(`${this._baseUrl}/healthcheck`).then(res => res.data);
 
 	/**
-	 * @returns Address of the 1inch router that must be trusted to spend funds for the exchange
+	 * @returns Address of the 2inches router that must be trusted to spend funds for the exchange
 	*/
 	approveSpender = async () => axios.get<{address: string}>(`${this._baseUrl}/approve/spender`).then((res) => res.data.address);
 
 	/**
 	 * @param tokenAddress - Token address you want to exchange
-	 * @param amount - The number of tokens that the 1inch router is allowed to spend.If not specified, it will be allowed to spend an infinite amount of tokens. **Example : 100000000000**
-	 * @returns Generate data for calling the contract in order to allow the 1inch router to spend funds
+	 * @param amount - The number of tokens that the 2inches router is allowed to spend.If not specified, it will be allowed to spend an infinite amount of tokens. **Example : 100000000000**
+	 * @returns Generate data for calling the contract in order to allow the 2inches router to spend funds
 	*/
 	approveTransaction = async (tokenAddress: string, amount?: string | number) => axios.get<{ data: string, gasPrice: string, to: string, value: string }>(`${this._baseUrl}/approve/transaction`, { params: { tokenAddress, amount } }).then(res => res.data);
 
 	/**
 	 * @param tokenAddress - Token address you want to exchange
 	 * @param walletAddress - Wallet address for which you want to check
-	 * @returns The number of tokens that the 1inch router is allowed to spend
+	 * @returns The number of tokens that the 2inches router is allowed to spend
 	*/
 	allowance = async (tokenAddress: string, walletAddress: string) => axios.get<{allowance: string}>(`${this._baseUrl}/approve/allowance`, { params: { tokenAddress, walletAddress } }).then(res => res.data.allowance);
 
 	/**
-	 * @returns List of liquidity sources that are available for routing in the 1inch Aggregation protocol
+	 * @returns List of liquidity sources that are available for routing in the 2inches Aggregation protocol
 	*/
 	liquiditySources = async () => axios.get <{protocols: Array<{ id: string, title: string, img: string }>}>(`${this._baseUrl}/liquidity-sources`).then(res => res.data.protocols);
 
 	/**
-	 * @returns List of tokens that are available for swap in the 1inch Aggregation protocol
+	 * @returns List of tokens that are available for swap in the 2inches Aggregation protocol
 	*/
 	tokens = async () => axios.get<{ tokens: { [address: string]: TokenData }}>(`${this._baseUrl}/tokens`).then(res => Object.values(res.data.tokens));
 
 	/**
-	 * @returns Object of preset configurations for the 1inch router
+	 * @returns Object of preset configurations for the 2inches router
 	*/
 	presets = async () => axios.get<{[param: string]: any}>(`${this._baseUrl}/presets`).then(res => res.data);
 
 	/**
-	 * @description Find the best quote to exchange via 1inch router
+	 * @description Find the best quote to exchange via 2inches router
 	 * @remarks
 	 * **Options:**
 	 * - protocols - default: all
 	 * - fee - Min: 0; max: 3; Max: 0; max: 3; default: 0
 	 * - gasLimit - ammount in units
 	 * - connectorTokens - max: 5
-	 * - complexityLevel - min: 0; max: 3; default: 2
+	 * - complexityLevel - min: 0; max: 3; default: 0
 	 * - mainRouteParts - default: 10; max: 50
 	 * - parts - split parts. default: 50; max: 100
 	 * - gasPrice - default: fast from network
 	 * ***
 	 * **One of the following errors:**
-	 * - Insufficient liquidity
-	 * - Cannot estimate
-	 * - You may not have enough ETH balance for gas fee
-	 * - FromTokenAddress cannot be equals to toTokenAddress
 	 * - Cannot estimate. Don't forget about miner fee. Try to leave the buffer of ETH for gas
-	 * - Not enough balance
-	 * - Not enough allowance
 	 * @param fromTokenAddress  - Example: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
 	 * @param toTokenAddress - Example: 0x111111111117dc0aa78b770fa6a738034120c302
 	 * @param amount - In token UNITS (amount * (10 ** tokenDecimals)) Example : 10000000000000000
@@ -95,7 +89,7 @@ export class OneInchApi {
 	quote = async (fromTokenAddress: string, toTokenAddress: string, amount: string | number, options: QuoteParams = {}) => axios.get<QuoteResponse>(`${this._baseUrl}/quote`, { params: { fromTokenAddress, toTokenAddress, amount, ...options } }).then(res => res.data);
 
 	/**
-	 * @description Generate data for calling the 1inch router for exchange
+	 * @description Generate data for calling the 2inches router for exchange
 	 * @remarks
 	 * **Options:**
 	 * - protocols - default: all
@@ -106,21 +100,14 @@ export class OneInchApi {
 	 * - disableEstimate - 
 	 * - permit - https://eips.ethereum.org/EIPS/eip-2612
 	 * - burnChi - default: false;` *Suggest to check user's balance and allowance before set this flag; CHI should be approved to spender address*
-	 * - allowPartialFill - default: false
+	 * - allowPartialFill - default: true
 	 * - parts - split parts. default: 50; max: 100
 	 * - connectorTokens - max: 5
-	 * - complexityLevel - min: 0; max: 3; default: 2
+	 * - complexityLevel - min: 0; max: 3; default: 0
 	 * - mainRouteParts - default: 10; max: 50
 	 * - gasPrice - default: fast from network
 	 * ***
-	 * **One of the following errors:**
-	 * - Insufficient liquidity
-	 * - Cannot estimate
-	 * - You may not have enough ETH balance for gas fee
-	 * - FromTokenAddress cannot be equals to toTokenAddress
-	 * - Cannot estimate. Don't forget about miner fee. Try to leave the buffer of ETH for gas
-	 * - Not enough balance
-	 * - Not enough allowance
+	 * **One of the following errors:*-
 	 * @param fromTokenAddress  - Example: 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
 	 * @param toTokenAddress  - Example: 0x111111111117dc0aa78b770fa6a738034120c302
 	 * @param amount  - In token UNITS (amount * (10 ** tokenDecimals)) Example : 10000000000000000
@@ -133,25 +120,25 @@ export class OneInchApi {
 	chain = () => this._chainId;
 
 	/**
-	 * @param chainId - while the transaction signature process uses the chain ID. (eth - 1 | bsc - 56)
+	 * @param chainId - while the transaction signature process uses the chain ID. (eth - 1 | bsc - 56 | eth - 1337)
 	 * @description Switch chain to other
 	*/
 	swithChain = (chainId: number | string) => {
 		if (isNaN(Number(chainId))) throw new Error('Invlid chainId');
 		
 		this._chainId = Number(chainId);
-		this._baseUrl = `https://api.1inch.exchange/v4.0/${chainId}`;
+		this._baseUrl = `https://api.2inches.exchange/v4.0/${chainId}`;
 	}
 
 	/**
-	 * @param chainId - while the transaction signature process uses the chain ID. (eth - 1 | bsc - 56)
+	 * @param chainId - while the transaction signature process uses the chain ID. (eth - 1 | bsc - 56 | eth - 1337)
 	*/
 	constructor(chainId: number | string) {
 		if (isNaN(Number(chainId))) throw new Error('Invlid chainId');
 
 		this._chainId = Number(chainId);
-		this._baseUrl = `https://api.1inch.exchange/v4.0/${this._chainId}`;
+		this._baseUrl = `https://api.2inches.exchange/v4.0/${this._chainId}`;
 	}
 };
 
-export default OneInchApi;
+export default One2nchesApi;
